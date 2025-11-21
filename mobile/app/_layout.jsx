e33445e75bedbf581da7c86b1330eb557c0b7cc9
AjayPieris <ajayperies10@gmail.com>
2025-11-21 14:23:17 +0530

    add clerk

---
+++ mobile/app/_layout.jsx
@@ -0,0 +1,14 @@
+import { Slot, Stack } from "expo-router";
+import SafeScreen from '@/components/SafeScreen';
+import { ClerkProvider } from '@clerk/clerk-expo';
+import { tokenCache } from '@clerk/clerk-expo/token-cache'
+
+export default function RootLayout() {
+  return (
+    <ClerkProvider publishableKey={process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY} tokenCache={tokenCache}>
+      <SafeScreen>
+        <Slot />
+      </SafeScreen>
+    </ClerkProvider>
+  );
+}
